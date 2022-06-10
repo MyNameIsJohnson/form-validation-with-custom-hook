3a435889d6aa84d5493e1d74cf331719e61c64cb
johnsonthieu <johnsonle2929@gmail.com>
2022-06-10 13:24:10 -0700

    added error message

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -2,23 +2,50 @@ import useValidate from "../hooks/validate";
 
 const BasicForm = (props) => {
   // Call useValidate and extract values from the result returned by useValidate. Since it returns an object we can use object destructuring to pull out those keys from the returned object and store the values in new constants. Extract the value and assign const alias.
-
+  const {
+    value: enteredName,
+    hasError: nameHasError,
+    isValid: nameIsValid,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: nameReset,
+    errorMessage: message = "Name must be added",
+  } = useValidate((value) => value.trim() !== "");
   // Declare a variable for formIsValid to a boolean
-
+  let formIsValid = false;
   // If the entered value being checked is true, set formIsValid to true
-
+  if (enteredName) {
+    formIsValid = true;
+  }
   // Declare formSubmissionHandler with event preventDefault.
-  // If entered value is not true, end function with a return
-  // reset value input by calling reset function from validate.js
-
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredName);
+    // If entered value is not true, end function with a return
+    if (!nameIsValid) {
+      return;
+    }
+    // reset value input by calling reset function from validate.js
+    nameReset("");
+  };
   // Conditionally render error message and add style to input
+  const nameInputClasses = nameHasError
+    ? "form-control invalid"
+    : "form-control";
 
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={nameInputClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={enteredName}
+            onBlur={nameBlurHandler}
+            onChange={nameChangeHandler}
+          />
+          {nameHasError && <p className="error-text">{message}</p>}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
@@ -30,7 +57,7 @@ const BasicForm = (props) => {
         <input type="text" id="name" />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- src/hooks/validate.js
+++ src/hooks/validate.js
@@ -2,14 +2,39 @@ import { useState } from "react";
 
 const useValidate = (validateValue) => {
   // Declare const state for enteredValue equal to empty string
+  const [enteredValue, setEnteredValue] = useState("");
   // Declare const state for isTouched equal to false
+  const [isTouched, setIsTouched] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   // Declare valueIsValid const that calls a function validateValue on enterdValue
+  const valueIsValid = validateValue(enteredValue);
   // Declare hasError const that evalutes is not valueIsValid and isTouched
+  const hasError = !valueIsValid && isTouched;
   // Declare valueChangeHandler event function and setEnteredValue to event target value
-  // Declare inputBlurHandler event function and setIsTouched to true
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+  // Declare inputBlurHandler event function and setIsTouched to true and call setErrorMessage
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+    setErrorMessage();
+  };
   // Declare reset function that setEnteredValue back to empty string and setIsTouched back to false
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
   // return object of keys and values
-  //  value to enteredValue, hasError to hasError, isValid to valueIsValue, valueChangeHandler to valueChangeHandler, inputBlurHandler to inputBlurHandler, reset to reset
+  return {
+    //  value to enteredValue, hasError to hasError, isValid to valueIsValue, valueChangeHandler to valueChangeHandler, inputBlurHandler to inputBlurHandler, reset to reset
+    value: enteredValue,
+    hasError,
+    isValid: valueIsValid,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+    errorMessage,
+  };
 };
 
 export default useValidate;
