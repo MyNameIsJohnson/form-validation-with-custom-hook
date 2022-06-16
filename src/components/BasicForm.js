9f7d91ee885a23480ede0028c087b4cf7595c323
johnson <johnsonle2929@gmail.com>
2022-06-16 11:06:04 -0700

    added last name validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -3,13 +3,23 @@ import useValidate from "../hooks/validate";
 const BasicForm = (props) => {
   // Call useValidate and extract values from the result returned by useValidate. Since it returns an object we can use object destructuring to pull out those keys from the returned object and store the values in new constants. Extract the value and assign const alias.
   const {
-    value: enteredName,
-    hasError: nameHasError,
-    isValid: nameIsValid,
-    valueChangeHandler: nameChangeHandler,
-    inputBlurHandler: nameBlurHandler,
-    reset: nameReset,
-    errorMessage: nameErrorMessage = "Name must be added",
+    value: enteredFirstName,
+    hasError: firstNameHasError,
+    isValid: firstNameIsValid,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: firstNameReset,
+    errorMessage: firstNameErrorMessage = "First Name must be added",
+  } = useValidate((value) => value.trim() !== "");
+
+  const {
+    value: enteredLastName,
+    hasError: lastNameHasError,
+    isValid: lastNameIsValid,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: lastNameReset,
+    errorMessage: lastNameErrorMessage = "Last Name Must Be Included",
   } = useValidate((value) => value.trim() !== "");
 
   const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
@@ -27,26 +37,31 @@ const BasicForm = (props) => {
   // Declare a variable for formIsValid to a boolean
   let formIsValid = false;
   // If the entered value being checked is true, set formIsValid to true
-  if (enteredName && enteredEmail) {
+  if (enteredFirstName && enteredLastName && enteredEmail) {
     formIsValid = true;
   }
   // Declare formSubmissionHandler with event preventDefault.
   const formSubmissionHandler = (e) => {
     e.preventDefault();
     // If entered value is not true, end function with a return
-    if (!nameIsValid && !emailIsValid) {
+    if (!firstNameIsValid && !lastNameIsValid && !emailIsValid) {
       return;
     }
     // reset value input by calling reset function from validate.js
-    nameReset("");
+    firstNameReset("");
+    lastNameReset("");
     emailReset("");
   };
   // Conditionally render error message and add style to input
-  const nameInputClasses = nameHasError
+  const nameInputClasses = firstNameHasError
     ? "form-control invalid"
     : "form-control";
 
   const emailInputClasses = emailHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const lastNameInputClasses = lastNameHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -58,15 +73,26 @@ const BasicForm = (props) => {
           <input
             type="text"
             id="name"
-            value={enteredName}
-            onBlur={nameBlurHandler}
-            onChange={nameChangeHandler}
+            value={enteredFirstName}
+            onBlur={firstNameBlurHandler}
+            onChange={firstNameChangeHandler}
           />
-          {nameHasError && <p className="error-text">{nameErrorMessage}</p>}
+          {firstNameHasError && (
+            <p className="error-text">{firstNameErrorMessage}</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={lastNameInputClasses}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={enteredLastName}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && (
+            <p className="error-text">{lastNameErrorMessage}</p>
+          )}
         </div>
       </div>
       <div className={emailInputClasses}>
